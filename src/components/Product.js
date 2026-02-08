582ebe96efbe66cca2404891831209434d6e9a7a
bikashdalapati-09 <bikashdalapati09@gmail.com>
2026-02-08 18:25:54 +0530

    add trending bannar on items above 4.5 ratings

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,13 +1,19 @@
 const Product = (props) => {
-    const {name, image, memory, price} = props.productObj
-    return (
-        <div className="product">
-            <img className="product-img" src={image}></img>
-            <h1 className="name">{name}</h1>
-            <h2 className="ram">{memory}</h2>
-            <h2 className="price">Price: ₹ {price}</h2>
-            <button className="buy">Buy now</button>
-        </div>
-    )
-}
+  const { name, image, memory, price, rating } = props.productObj;
+  return (
+    <div className="product">
+      {rating > 4.5 && <div className="flex justify-start">
+        <span className="bg-yellow-500 text-white rounded-md pl-1.5 pr-1 pb-0.5 m-0.5 text-xs font-semibold">
+          Trending 🔥
+        </span>
+      </div>}
+
+      <img className="product-img" src={image}></img>
+      <h1 className="name">{name}</h1>
+      <h2 className="ram">{memory}</h2>
+      <h2 className="price">Price: ₹ {price}</h2>
+      <button className="buy">Buy now</button>
+    </div>
+  );
+};
 export default Product;
