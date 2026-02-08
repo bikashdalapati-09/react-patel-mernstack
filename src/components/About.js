8090f97b06789f33c6d020284e5b9944b20e7f90
bikashdalapati-09 <bikashdalapati09@gmail.com>
2026-02-08 17:48:07 +0530

    display developer in info using github profile fetch api

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from "react"
+import Skeleton from "./Skeleton";
 
 function About() {
+  const [data, setData] = useState(null)
+
+
+  useEffect( () => {
+    getData();
+  }, [])
+  const getData = async () => {
+    const info = await fetch('https://api.github.com/users/bikashdalapati-09');
+    const resData = await info.json()
+    setData(resData);
+    console.log(resData);
+  }
+  
+  
+  if(data === null){
+    return <Skeleton/>
+  }
+
   return (
-    <div>About</div>
-  )
+    <div className="">
+        <>
+          <img src={data.avatar_url} width="100" />
+          <p>Name: {data.name}</p>
+          <p>Username: {data.login}</p>
+        </>
+    </div>
+  );
 }
 
 export default About
